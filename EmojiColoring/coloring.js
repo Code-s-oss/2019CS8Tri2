dedbc922f68e45f0215aee12c924f8d02e510dea
Code-s-oss <tkalotis24@nscds.org>
2019-12-17 19:08:24 +0000

    followed class

---
+++ EmojiColoring/coloring.js
@@ -0,0 +1,10 @@
+$(".pupilImage").click(
+    function(){
+        alert("I wish it was Friday")
+    }
+);
+$(".mouthImage").click(
+    function(){
+        var clickedMouth = $(this).css("background-image");
+    }
+);
