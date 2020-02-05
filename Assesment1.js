b236db334118efc4ffa4112e8e5a2cd35a468555
Code-s-oss <tkalotis24@nscds.org>
2020-02-05 17:17:39 +0000

    did assesmnet

---
+++ Assesment1.js
@@ -0,0 +1,10 @@
+$(".thing type1").click(
+    function(){
+        alert("Hello")
+    }
+);
+$(".thing type1").click(
+    function(){
+        var clickedThing = $(this).css("background-image");
+    }
+);
